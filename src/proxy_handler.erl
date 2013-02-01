bf832f00f17d8f5533b62ccd9242cafc22916291
Paul Bonser <pib@paulbonser.com>
2013-01-31 20:01:07 -0600

    Create proxy gen_server for the actual handling of proxy request

--- src/proxy_handler.erl
+++ src/proxy_handler.erl
@@ -2,19 +2,15 @@
 -behavior(cowboy_loop_handler).
 -export([init/3, info/3, terminate/3]).
 
-proxy_request(Req) ->
-    {Method, _} = cowboy_req:method(Req),
-    {Host, _} = cowboy_req:host_url(Req),
-    {Url, _} = cowboy_req:url(Req),
-    lager:info("~p ~p", [Method, Url]),
-    self() ! {reply, "OK"}.
-
 init({tcp, http}, Req, _) ->
-    proxy_request(Req),
+    proxy_server:proxy(Req, self()),
     {loop, Req, undefined, 600, hibernate}.
 
 info({reply, Body}, Req, State) ->
     {ok, Req2} = cowboy_req:reply(200, [], Body, Req),
+    {ok, Req2, State};
+info({not_allowed, Allowed}, Req, State) ->
+    {ok, Req2} = cowboy_req:reply(405, [{<<"allowed">>, Allowed}], Req),
     {ok, Req2, State};
 info(Message, Req, State) ->
     {loop, Req, State, hibernate}.

afd6a14f5b7bf7c92417e7059584cd88efb3ae4f
Paul Bonser <pib@paulbonser.com>
2013-03-06 16:46:35 -0600

    Handle outgoing request errors more gracefully

--- src/qdecp_cowboy_handler.erl
+++ src/qdecp_cowboy_handler.erl
@@ -26,18 +26,29 @@ start_request(Req) ->
     lager:debug("~p ~p ~p", [Ip, Method, Url]),
     case Method of
         <<"GET">> ->
-            {ok, ReqId} = send_req(Ip, get, Url, Headers, []),
-            {loop, Req, #state{req_id=ReqId}, hibernate};
+            case send_req(Ip, get, Url, Headers, [], 10) of
+                {ok, ReqId} ->
+                    {loop, Req, #state{req_id=ReqId}, hibernate};
+                {error, Code} ->
+                    {ok, Req2} = cowboy_req:reply(Code, [], Req),
+                    {shutdown, Req2, #state{}}
+            end;
+                    
         <<"POST">> ->
             {ok, Body, _} = cowboy_req:body(Req),
-            {ok, ReqId} = send_req(Ip, post, Url, Headers, Body),
-            {loop, Req, #state{req_id=ReqId}, hibernate};
+            case send_req(Ip, post, Url, Headers, Body, 10) of
+                 {ok, ReqId} ->
+                    {loop, Req, #state{req_id=ReqId}, hibernate};
+                {error, Code} ->
+                    {ok, Req2} = cowboy_req:reply(Code, [], Req),
+                    {shutdown, Req2, #state{}}
+            end;
         <<"CONNECT">> ->
             {upgrade, protocol, qdecp_connect_proxy};
         _ ->
             {ok, Req2} = cowboy_req:reply(405, [{<<"allow">>, <<"GET, POST, CONNECT">>}], Req),
             qdecp_stats:log_event({response, '405'}),
-            {ok, Req2, #state{}}
+            {shutdown, Req2, #state{}}
     end.
 
 info({ibrowse_async_headers, ReqId, Code, Headers}, Req, State) ->
@@ -108,7 +119,10 @@ response_headers(Headers) ->
         {Name, Val} <- Headers, Name =/= "Content-Length",
         Name =/= "Transfer-Encoding"].
 
-send_req(Ip, Method, Url, Headers, Body) ->
+send_req(_, _, Url, _, _, 0) ->
+    lager:error("Too many retries, giving up on connection to ~p", [Url]),
+    {error, 503};
+send_req(Ip, Method, Url, Headers, Body, Retries) ->
     case ibrowse:send_req(binary_to_list(Url), Headers, Method, Body,
                           [{stream_to, self()},{socket_options, [{ip, Ip}]}]) of
         {ibrowse_req_id, ReqId} -> 
@@ -116,6 +130,16 @@ send_req(Ip, Method, Url, Headers, Body) ->
             {ok, ReqId};
         {error, retry_later} ->
             qdecp_stats:log_event({request_retry, Method}),
-            lager:error("Got retry later, retrying."),
-            send_req(Ip, Method, Url, Headers, Body)
+            lager:error("Got retry later for ~p, retrying.", [Url]),
+            send_req(Ip, Method, Url, Headers, Body, Retries - 1);
+        {error, {conn_failed, {error, nxdomain}}} ->
+            lager:error("Connection to ~p failed, domain does not exist", [Url]),
+            {error, 502};
+        {error, {conn_failed, Err}} ->
+            qdecp_stats:log_event({request_retry, Method}),
+            lager:error("Connection to ~p failed with ~p, retrying", [Url, Err]),
+            send_req(Ip, Method, Url, Headers, Body, Retries - 1);
+        Else ->
+            lager:error("Failed to send request to ~p: ~p", [Url, Else]),
+            {error, 500}
     end.
